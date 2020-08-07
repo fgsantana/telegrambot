6c6ece4a0a1a135371eb904e873b277556d47d78
felipe gomes <felipinhogoma@gmail.com>
2020-08-07 15:20:33 -0300

    Initial structure/3

--- src/main/java/com/test/telegrambot/client/Client.java
+++ src/main/java/com/test/telegrambot/client/Client.java
@@ -1,15 +1,13 @@
 package com.test.telegrambot.client;
 
 import com.test.telegrambot.dto.ResultUpdate;
-import com.test.telegrambot.dto.Update;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 
 
-@FeignClient(url = "https://api.telegram.org/bot1206975479:AAHriKQuVXB-0nX5hFtLhcKmX-cXpavxnOQ", name= "telegram")
+@FeignClient(url = "${telegram.api}${telegram.token}", name= "telegram")
 public interface Client {
 
     @GetMapping("/getUpdates")
